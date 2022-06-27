65ff0ca89af674735693b84ac6006fd1f48118d8
lrabbit <15821745523@163.com>
2022-06-27 23:44:51 +0800

    feat: test cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Add Express
 const express = require("express");
-
+const cors = require('cors');
 // Initialize Express
 const app = express();
-
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 // Create GET request
 app.get("/", (req, res) => {
   res.send({
